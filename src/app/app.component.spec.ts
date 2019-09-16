99e79cbe271fcb559fbe4b9147d94b46813112c8
José Raddaoui Marín <raddaouimarin@gmail.com>
2019-09-16 20:34:27 +0200

    Use stub component in app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,30 @@
+import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    @Component({selector: 'app-navigation', template: ''})
+    class NavigationStubComponent { }
+
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
-        MatButtonModule,
-        MatExpansionModule,
         MatIconModule,
-        MatListModule,
         MatSidenavModule,
         MatToolbarModule,
       ],
       declarations: [
         AppComponent,
-        NavigationComponent,
+        NavigationStubComponent,
       ],
     }).compileComponents();
   }));
